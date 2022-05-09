cfc234205b756fcd6b11095125c0f4e707761c74
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-09 11:24:17 +0200

    added hero icons

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,19 @@
 import Image from "next/image";
+import {
+    BellIcon,
+    ChatIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridIcon
+} from "@heroicons/react/solid";
+
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppingCartIcon
+} from "@heroicons/react/outline"
 
 function Header(){
     return(
@@ -6,8 +21,13 @@ function Header(){
  <h1>Header</h1>
 
  {/* left */}
- <div>
+ <div className="flex items-center">
      <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed"/>
+     <div className="flex">
+     <SearchIcon className="h-6"/>
+     <input type="text" placeholder="Search Facebook"/>
+     </div>
+    
  </div>
  {/* center */}
  {/* right */}
