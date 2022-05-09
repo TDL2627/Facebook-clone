19c7a3cfdbbaa000f0421f6e624a444ae35e52cb
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-09 13:04:19 +0200

    finished head for now

--- components/Header.js
+++ components/Header.js
@@ -24,7 +24,7 @@ function Header(){
      <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed"/>
      <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
      <SearchIcon className="h-6 text-gray-600"/>
-     <input className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 " type="text" placeholder="Search Facebook"/>
+     <input className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 " type="text" placeholder="Search Facebook"/>
      </div>
     
  </div>
@@ -40,6 +40,16 @@ function Header(){
      </div>
  </div>
  {/* right */}
+ <div className="flex items-center sm:space-x-2 justify-end">
+{/* profile pic */}
+<p className="whitespace-nowrap font-semi-bold pr-3">Peter Pan</p>
+<ViewGridIcon className="icon"/>
+<ChatIcon className="icon"/>
+<BellIcon className="icon"/>
+<ChevronDownIcon className="icon"/>
+ </div>
+
+
         </div>
     )
 }
