70133d0fc1513b5f02a44a7fc4e4efde7c045734
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-10 10:20:45 +0200

    added post form

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import {useSession } from "next-auth/react";
+import {EmojiHappyIcon } from "@heroicons/react/outline";
+import {CameraIcon,VideoCameraIcon} from "@heroicons/react/solid";
 
 function InputBox(){
 
     const { data: session, status } = useSession();
 
+   const sendPost = (e)=>{
+       e.preventDefault();
+   }
 
-    
     return(
         <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
       <div className="flex  space-x-4 p-4 items-center">
@@ -22,8 +26,27 @@ function InputBox(){
               className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
                type="text" 
               placeholder={`Whats on your mind ${session.user.name}?`} />
-    <button hidden type="submit">Submit</button>
+
+
+    <button onClick={sendPost} hidden type="submit">Submit</button>
           </form>
+      </div>
+
+      <div className="flex justify-evenly p-3 border-t">
+      <div className="inputIcon">
+          <VideoCameraIcon className="h-7 text-red-500 "/>
+          <p className="text-xs sm:text-sm xl:text-base ">Live Video</p>
+      </div>
+
+      <div className="inputIcon">
+       <CameraIcon className="h-7 text-green-400"/>
+       <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+      </div>
+      <div className="inputIcon">
+          <EmojiHappyIcon className="h-7 text-yellow-300"/>
+          <p className="text-xs sm:text-sm xl:text-base" >Feeling/Activity</p>
+      </div>
+
       </div>
         </div>
     )
