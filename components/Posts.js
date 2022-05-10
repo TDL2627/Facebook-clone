11dea32729e103e445b6b40ccc269d575bf40e21
TDL2627 <the.dark.lord.2627@gmail.com>
2022-05-10 16:43:23 +0200

    bug fix

--- components/Posts.js
+++ components/Posts.js
@@ -1,26 +1,48 @@
-import {useCollection} from "react-firebase-hooks/firestore";
-import Post from "./Post"
-import db from "../firebase";
-import{collection, addDoc, serverTimestamp, doc, updateDoc} from "firebase/firestore";
+import React from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  doc,
+  updateDoc,
+} from "@firebase/firestore";
+import  db  from "../firebase";
+import Post from "./Post";
 
-function Posts (){
-    const [realtimePosts]= useCollection(
-        db.collection("posts").orderBy("timestamp", "desc")
-    );
-    return(
-        <div>
-      {realtimePosts?.docs.map(post =>(
-          <Post
-          key={post.id}
-          name={post.data().name}
-          message={post.data().message}
-          email={post.data().email}
-          timestamp={post.data().timestamp}
-          image={post.data().image}
-          postImage={post.data().postImage}
-          />
-      ))}
-        </div>
-    )
+export default function Posts({ postsList }) {
+  const postCollections = collection(db, "posts");
+  const [posts] = useCollection(postCollections);
+
+  console.log("postList from server", postsList);
+
+  return (
+    <div>
+      {posts
+        ? posts.docs?.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                name={post.data().name}
+                message={post.data().message}
+                email={post.data().email}
+                timeStamp={post.data().timeStamp}
+                image={post.data().image}
+                postImage={post.data().postImage}
+              />
+            );
+          })
+        : postsList.map((post) => {
+            <Post
+              key={post.id}
+              name={post.name}
+              message={post.message}
+              email={post.email}
+              timeStamp={post.timeStamp}
+              image={post.image}
+              postImage={post.postImage}
+            />;
+          })}
+    </div>
+  );
 }
-export default Posts;
